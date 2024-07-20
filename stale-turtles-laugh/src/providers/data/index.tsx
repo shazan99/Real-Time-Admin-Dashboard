dd5a1c54e2b5262f4f45a96c497acd5742b8f639
shazan99 <shazanahmed786@gmail.com>
2024-07-20 17:25:12 +0530

    Created GraphQL Web Sockets to listen to changes

--- stale-turtles-laugh/src/providers/data/index.tsx
+++ stale-turtles-laugh/src/providers/data/index.tsx
@@ -1,6 +1,14 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import graphqlDataProvider, {
+    GraphQLClient,
+    liveProvider as graphqlLiveProvider
+} from "@refinedev/nestjs-query";
 import { fetchWrapper } from "./fetch-wrapper";
+import { create } from "domain";
+import { createClient } from "graphql-ws";
+
+export const API_BASE_URL = 'https://api.crm.refine.dev'
 export const API_URL = 'https://api.crm.refine.dev'
+export const WS_URL = 'wss://api.crm.refine.dev/graphql'
 
 export const client = new GraphQLClient(API_URL, {
     fetch: (url: string, options: RequestInit) => {
@@ -12,3 +20,21 @@ export const client = new GraphQLClient(API_URL, {
     }
 })
 
+//Web Socket GraphQL client to listen to changes
+export const wsClient = typeof window !== "undefined"
+    ? createClient({
+        url: WS_URL,
+        connectionParams: () => {
+            const accessToken = localStorage.getItem("access_token");
+
+            return {
+                headers: {
+                    Authorization: `Bearer ${accessToken}`,
+                }
+            };
+        }
+    })
+    : undefined;
+
+export const dataProvider = graphqlDataProvider(client);
+export const liveProvider = wsClient ? graphqlLiveProvider(wsClient) : undefined;
