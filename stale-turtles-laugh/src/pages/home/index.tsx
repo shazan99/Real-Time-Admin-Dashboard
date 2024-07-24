4fba3295e5e792a64fec5233b7f44c32e4b2497e
shazan99 <shazanahmed786@gmail.com>
2024-07-24 22:54:37 +0530

    Extended the size of Deals Chart to take up 2/3rd of the screen and created 3 more cards on top.

--- stale-turtles-laugh/src/pages/home/index.tsx
+++ stale-turtles-laugh/src/pages/home/index.tsx
@@ -4,11 +4,23 @@ import { Col, Row } from "antd"
 export const Home = () => {
   return (
     <div>
+      <Row gutter={[32,32]}>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          DashboardTotalCountCard
+        </Col>
+      </Row>
+
       <Row gutter={[32, 32]} style={{marginTop: '32px'}}>
         <Col xs={24} sm={24} xl={8} style={{ height:'460px' }}>
           <UpcomingEvents />
         </Col>
-        <Col xs={24} sm={24} xl={8} style={{ height:'460px' }}>
+        <Col xs={24} sm={24} xl={16} style={{ height:'460px' }}>
           <DealsChart />
         </Col>
       </Row>
