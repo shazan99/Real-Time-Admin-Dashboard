45f2698263be4d82c17910fb99a2d40834d018b8
shazan99 <shazanahmed786@gmail.com>
2024-07-24 18:01:52 +0530

    Updated the avatar icon and it's position in the homepage

--- stale-turtles-laugh/src/components/layouts/current-user.tsx
+++ stale-turtles-laugh/src/components/layouts/current-user.tsx
@@ -5,6 +5,7 @@ import type { User } from '@/graphql/schema.types'
 import { Text } from '../text'
 import { SettingOutlined } from '@ant-design/icons'
 import { useState } from 'react'
+import { AccountSettings } from './account-settings'
 
 
 const CurrentUser = () => {
@@ -52,9 +53,12 @@ const CurrentUser = () => {
         style={{ cursor: 'pointer'}}/>
         
     </Popover>
-    {user && (<AccountSettings 
-    
-    />
+    {user && (
+        <AccountSettings 
+            opened={isOpen}
+            setOpened={setIsOpen}
+            userId={user.id}    
+        />
     )}
     </>
   )
