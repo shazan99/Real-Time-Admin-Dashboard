46f4dbd25bef080218c5a3c43e26c447e7c39352
shazan99 <shazanahmed786@gmail.com>
2024-07-28 19:17:24 +0530

    Developed the logic to render the company list along with search functionality.

--- stale-turtles-laugh/src/pages/index.ts
+++ stale-turtles-laugh/src/pages/index.ts
@@ -2,4 +2,5 @@ export * from './home';
 export * from './forgotPassword';
 export * from './login';
 export * from './register';
-export * from './companyList';
+export * from './company/list';
+export * from './company/create';
