803072cdaef6451e219a9a729cfc60b8af2048b6
shazan99 <shazanahmed786@gmail.com>
2024-07-30 23:06:07 +0530

    Added Company Contacts Table inside the Edit view of a particular company.

--- stale-turtles-laugh/src/pages/company/edit.tsx
+++ stale-turtles-laugh/src/pages/company/edit.tsx
@@ -8,6 +8,7 @@ import { UsersSelectQuery } from "@/graphql/types";
 import { USERS_SELECT_QUERY } from "@/graphql/queries";
 import SelectOptionWithAvatar from "@/components/select-option-with-avatar";
 import { businessTypeOptions, companySizeOptions, industryOptions } from "@/constants";
+import { CompanyContactsTable } from "./contacts-table";
 
 const EditPage = () => {
 
@@ -101,6 +102,9 @@ const EditPage = () => {
                         </Form.Item>
                     </Form>
                 </Edit>
+            </Col>
+            <Col xs={24} xl={12}>
+                <CompanyContactsTable />               
             </Col>
         </Row> 
     </div>
