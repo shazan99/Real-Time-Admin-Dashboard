62294f6507fe08fca7853c3bfc454c44d32c234d
shazan99 <shazanahmed786@gmail.com>
2024-07-30 19:30:02 +0530

    Edit page created for company and added avatar and Sales owner dropdown to UI.

--- stale-turtles-laugh/src/App.tsx
+++ stale-turtles-laugh/src/App.tsx
@@ -17,6 +17,7 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Layout from "./components/layouts";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
 
 function App() {
   return (
@@ -60,6 +61,7 @@ function App() {
                     <Route path="/companies">
                       <Route index element={<CompanyList />} />
                       <Route path="new" element={<Create />} />
+                      <Route path="edit/:id" element={<Edit />} />
                     </Route>
                   </Route>
                 </Routes>
