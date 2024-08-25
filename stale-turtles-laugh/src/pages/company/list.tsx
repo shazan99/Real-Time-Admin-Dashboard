ed79372142b0aff9884bd64b1f754cfb4b9382f6
shazan99 <shazanahmed786@gmail.com>
2024-08-25 10:25:25 +0530

    Fixed typescript errors and changed the CompnayList component

--- stale-turtles-laugh/src/pages/company/list.tsx
+++ stale-turtles-laugh/src/pages/company/list.tsx
@@ -1,127 +1,154 @@
-import CustomAvatar from '@/components/custom-avatar';
-import { Text } from '@/components/text';
-import { COMPANIES_LIST_QUERY } from '@/graphql/queries';
-import { Company } from '@/graphql/schema.types';
-import { CompaniesListQuery } from '@/graphql/types';
-import { currencyNumber } from '@/utilities/utilities';
-import { SearchOutlined } from '@ant-design/icons';
-import { CreateButton, EditButton, DeleteButton, FilterDropdown, List, useTable } from '@refinedev/antd';
-import { getDefaultFilter, HttpError, useGo } from '@refinedev/core';
-import { GetFieldsFromList } from '@refinedev/nestjs-query';
-import { Input, Space, Table } from 'antd';
+import React from "react";
 
-/* @ts-ignore */
-export const CompanyList = ({ children}: React.PropsWithChildren) => {
+import {
+  CreateButton,
+  DeleteButton,
+  EditButton,
+  FilterDropdown,
+  List,
+  useTable,
+} from "@refinedev/antd";
+import { getDefaultFilter, type HttpError, useGo } from "@refinedev/core";
+import type { GetFieldsFromList } from "@refinedev/nestjs-query";
 
+import { SearchOutlined } from "@ant-design/icons";
+import { Input, Space, Table } from "antd";
+
+import type { CompaniesListQuery } from "@/graphql/types";
+import { currencyNumber } from "@/utilities/utilities";
+
+import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
+import CustomAvatar from "@/components/custom-avatar";
+import { Text } from "@/components";
+
+type Company = GetFieldsFromList<CompaniesListQuery>;
+
+export const CompanyList = ({ children }: React.PropsWithChildren) => {
   const go = useGo();
-  const { tableProps, filters } = useTable<
-    GetFieldsFromList<CompaniesListQuery>,
-    HttpError,
-    GetFieldsFromList<CompaniesListQuery>
-    >({
-    resource: 'companies',
+
+  const { tableProps, filters } = useTable<Company, HttpError, Company>({
+    resource: "companies",
     onSearch: (values) => {
-      return[
+      return [
         {
-          field: 'name',
-          operator: 'contains',
-          values: value.name
-        }
-      ]
-    },
-    pagination: {
-      pageSize: 12
+          field: "name",
+          operator: "contains",
+          value: values.name,
+        },
+      ];
     },
     sorters: {
       initial: [
         {
-          field: 'createdAt',
-          order: 'desc'
-        }
-      ]
+          field: "createdAt",
+          order: "desc",
+        },
+      ],
     },
     filters: {
       initial: [
         {
-          field: 'name',
-          operator: 'contains',
-          value: undefined
-        }
-      ]
+          field: "name",
+          operator: "contains",
+          value: undefined,
+        },
+      ],
+    },
+    pagination: {
+      pageSize: 12,
     },
     meta: {
-      gqlQuery: COMPANIES_LIST_QUERY
-    }
-  })
+      gqlQuery: COMPANIES_LIST_QUERY,
+    },
+  });
+
   return (
-  <div>
-    <List
-      breadcrumb={false}
-      headerButtons={() => (
-        <CreateButton
-          onClick={() => {
-            go({
-              to: {
-                resource: 'companies',
-                action: 'create'
-              },
-              options: {
-                keepQuery: true
-              },
-              type: 'replace'
-            })
-          }} />
-      )}
-    >
-      <Table 
-        {...tableProps}
-        pagination={{
-          ...tableProps.pagination,
+    <div className="page-container">
+      <List
+        breadcrumb={false}
+        headerButtons={() => {
+          return (
+            <CreateButton
+              onClick={() => {
+                // modal is a opening from the url (/companies/new)
+                // to open modal we need to navigate to the create page (/companies/new)
+                // we are using `go` function because we want to keep the query params
+                go({
+                  to: {
+                    resource: "companies",
+                    action: "create",
+                  },
+                  options: {
+                    keepQuery: true,
+                  },
+                  type: "replace",
+                });
+              }}
+            />
+          );
         }}
       >
-        <Table.Column<Company>
-          dataIndex="name"
-          title="Company Title"
-          defaultFilteredValue={getDefaultFilter('id', filters)}
-          filterIcon={<SearchOutlined />}
-          filterDropdown={(props) => (
-            <FilterDropdown {...props}>
-              <Input placeholder="Search Company"/>
-            </FilterDropdown>
-          )}
-          render={(value, record) => (
-            <Space>
-              <CustomAvatar shape="circle" name={record.name} src={record.avatarUrl} />
-              <Text style={{ whiteSpace: 'nowrap'  }}>
-                {record.name}
-              </Text>
-            </Space>
-          )}
-        />
-        <Table.Column<Company>
-          dataIndex="totalRevenue"
-          title="Open deals amount"
-          render={(value, company) => (
-            <Text>
-              {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
-            </Text>
-          )} 
-        />
-        <Table.Column<Company>
-          dataIndex="id"
-          title="Actions"
-          fixed="right"
-          render={(value) => (
-           <Space>
-            <EditButton hideText size="small" recordItemId={value} />
-            <DeleteButton hideText size="small" recordItemId={value} />
-           </Space>
-          )} 
-        />
-      </Table>
-    </List>
-    {children}
-  </div>
-  )
-}
+        <Table
+          {...tableProps}
+          pagination={{
+            ...tableProps.pagination,
+          }}
+        >
+          <Table.Column<Company>
+            dataIndex="name"
+            title="Company title"
+            defaultFilteredValue={getDefaultFilter("id", filters)}
+            filterIcon={<SearchOutlined />}
+            filterDropdown={(props) => (
+              <FilterDropdown {...props}>
+                <Input placeholder="Search Company" />
+              </FilterDropdown>
+            )}
+            render={(_, record) => {
+              return (
+                <Space>
+                  <CustomAvatar
+                    shape="square"
+                    name={record.name}
+                    src={record.avatarUrl}
+                  />
+                  <Text
+                    style={{
+                      whiteSpace: "nowrap",
+                    }}
+                  >
+                    {record.name}
+                  </Text>
+                </Space>
+              );
+            }}
+          />
+          <Table.Column<Company>
+            dataIndex={"totalRevenue"}
+            title="Open deals amount"
+            render={(_, company) => {
+              return (
+                <Text>
+                  {currencyNumber(company?.dealsAggregate?.[0].sum?.value || 0)}
+                </Text>
+              );
+            }}
+          />
+          <Table.Column<Company>
+            fixed="right"
+            dataIndex="id"
+            title="Actions"
+            render={(value) => (
+              <Space>
+                <EditButton hideText size="small" recordItemId={value} />
 
+                <DeleteButton hideText size="small" recordItemId={value} />
+              </Space>
+            )}
+          />
+        </Table>
+      </List>
+      {children}
+    </div>
+  );
+};
