a1db258fda74e054408a86d78c38bfd5924570b8
shazan99 <shazanahmed786@gmail.com>
2024-08-25 22:44:04 +0530

    Added Vercel Analytics for monitoring the app

--- stale-turtles-laugh/src/index.tsx
+++ stale-turtles-laugh/src/index.tsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
+import { inject } from '@vercel/analytics';
+ 
+inject();
+
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
 
