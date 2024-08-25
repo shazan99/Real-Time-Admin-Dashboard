a4066994f6f35c285a4a805cbd30eda0801efded
shazan99 <shazanahmed786@gmail.com>
2024-08-25 08:52:34 +0530

    Fixed TypeScript errors

--- stale-turtles-laugh/src/pages/tasks/list.tsx
+++ stale-turtles-laugh/src/pages/tasks/list.tsx
@@ -1,19 +1,19 @@
 import { KanbanColumnSkeleton, ProjectCardSkeleton } from '@/components'
 import { KanbanAddCardButton } from '@/components/tasks/kanban/add-card-button'
 import { KanbanBoardContainer, KanbanBoard } from '@/components/tasks/kanban/board'
-import ProjectCard, { ProjectCardMemo } from '@/components/tasks/kanban/card'
+import { ProjectCardMemo } from '@/components/tasks/kanban/card'
 import KanbanColumn from '@/components/tasks/kanban/column'
 import KanbanItem from '@/components/tasks/kanban/item'
 import { UPDATE_TASK_STAGE_MUTATION } from '@/graphql/mutations'
 import { TASK_STAGES_QUERY, TASKS_QUERY } from '@/graphql/queries'
-import { TaskStage } from '@/graphql/schema.types'
-import { TasksQuery } from '@/graphql/types'
+import { TasksQuery, TaskStagesQuery } from '@/graphql/types'
 import { DragEndEvent } from '@dnd-kit/core'
 import { useList, useNavigation, useUpdate } from '@refinedev/core'
 import { GetFieldsFromList } from '@refinedev/nestjs-query'
 import React from 'react'
 
-
+type Task = GetFieldsFromList<TasksQuery>
+type TaskStage = GetFieldsFromList<TaskStagesQuery> & { tasks: Task[] }
 const List = ({ children }: React.PropsWithChildren) => {
 
   const { replace } = useNavigation();
